071b3c40a7b6c312b1a0871829fc614935c36bd1
shuang.kou <koushuangbwcx@163.com>
2020-06-06 15:59:52 +0800

    [v3.0]refractor format log

--- rpc-framework-simple/src/main/java/github/javaguide/registry/ZkServiceDiscovery.java
+++ rpc-framework-simple/src/main/java/github/javaguide/registry/ZkServiceDiscovery.java
@@ -19,7 +19,7 @@ public class ZkServiceDiscovery implements ServiceDiscovery {
         // TODO(shuang.kou):feat: 负载均衡
         // 这里直接去了第一个找到的服务地址,eg:127.0.0.1:9999
         String serviceAddress = CuratorUtils.getChildrenNodes(serviceName).get(0);
-        log.info("成功找到服务地址:{}", serviceAddress);
+        log.info("成功找到服务地址:[{}]", serviceAddress);
         String[] socketAddressArray = serviceAddress.split(":");
         String host = socketAddressArray[0];
         int port = Integer.parseInt(socketAddressArray[1]);
--- rpc-framework-simple/src/main/java/github/javaguide/remoting/transport/netty/client/NettyClientTransport.java
+++ rpc-framework-simple/src/main/java/github/javaguide/remoting/transport/netty/client/NettyClientTransport.java
@@ -40,7 +40,7 @@ public class NettyClientTransport implements ClientTransport {
             unprocessedRequests.put(rpcRequest.getRequestId(), resultFuture);
             channel.writeAndFlush(rpcRequest).addListener((ChannelFutureListener) future -> {
                 if (future.isSuccess()) {
-                    log.info("client send message: {}", rpcRequest);
+                    log.info("client send message: [{}]", rpcRequest);
                 } else {
                     future.channel().close();
                     resultFuture.completeExceptionally(future.cause());
