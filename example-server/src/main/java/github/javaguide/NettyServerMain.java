e09d27a80062ba6774f1eac7625ba8b676d69c25
MrBigThree <812496582@qq.com>
2020-06-19 18:46:41 +0800

    回退文件

--- example-server/src/main/java/github/javaguide/NettyServerMain.java
+++ example-server/src/main/java/github/javaguide/NettyServerMain.java
@@ -9,7 +9,6 @@ import github.javaguide.remoting.transport.netty.server.NettyServer;
  */
 public class NettyServerMain {
     public static void main(String[] args) {
-
         HelloService helloService = new HelloServiceImpl();
         NettyServer nettyServer = new NettyServer("127.0.0.1", 9999);
         nettyServer.publishService(helloService, HelloService.class);
