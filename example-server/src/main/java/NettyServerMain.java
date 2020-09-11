8b33f5e037cde65940051bbf2c725cca028b1249
busysha <shaat1990@163.com>
2020-09-11 11:52:13 +0800

    [feat] server test via annotation

--- example-server/src/main/java/NettyServerMain.java
+++ example-server/src/main/java/NettyServerMain.java
@@ -1,8 +1,5 @@
-import github.javaguide.HelloService;
 import github.javaguide.annotation.RpcScan;
-import github.javaguide.entity.RpcServiceProperties;
 import github.javaguide.remoting.transport.netty.server.NettyServer;
-import github.javaguide.serviceimpl.HelloServiceImpl2;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -11,17 +8,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @author shuang.kou
  * @createTime 2020年05月10日 07:25:00
  */
-@RpcScan(basePackage = {"github.javaguide.serviceimpl"})
+@RpcScan(basePackage = {"github.javaguide"})
 public class NettyServerMain {
     public static void main(String[] args) {
         // Register service via annotation
-        new AnnotationConfigApplicationContext(NettyServerMain.class);
-        NettyServer nettyServer = new NettyServer();
-        // Register service manually
-        HelloService helloService2 = new HelloServiceImpl2();
-        RpcServiceProperties rpcServiceProperties = RpcServiceProperties.builder()
-                .group("test2").version("version2").build();
-        nettyServer.registerService(helloService2, rpcServiceProperties);
+        AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext(NettyServerMain.class);
+        NettyServer nettyServer = (NettyServer) applicationContext.getBean("nettyServer");
         nettyServer.start();
     }
 }
