11b4c987c2aaa77a109c1914c1ec308748053d3e
TangMinXuan <hacfun741950370@gmail.com>
2020-11-09 20:38:53 +0800

    [add] Add support for protostuff

---
+++ rpc-framework-simple/src/main/java/github/javaguide/serialize/protostuff/ProtostuffSerializer.java
@@ -0,0 +1,42 @@
+package github.javaguide.serialize.protostuff;
+
+import github.javaguide.remoting.dto.RpcMessage;
+import github.javaguide.serialize.Serializer;
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+/**
+ * @author TangMinXuan
+ * @createTime 2020年11月09日 20:13
+ */
+public class ProtostuffSerializer implements Serializer {
+
+    /**
+     * Avoid re applying buffer space every time serialization
+     */
+    private static LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public byte[] serialize(Object obj) {
+        Class<?> clazz = obj.getClass();
+        Schema schema = RuntimeSchema.getSchema(clazz);
+        byte[] bytes;
+        try {
+            bytes = ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } finally {
+            buffer.clear();
+        }
+        return bytes;
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        T obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        return obj;
+    }
+}
