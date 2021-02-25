432883aa291132b8e8634ebedd6b6554f29d8e04
ChoKhoOu <1009127844@qq.com>
2021-02-25 18:07:38 +0800

    optimized SingletonFactory

--- rpc-framework-common/src/main/java/github/javaguide/factory/SingletonFactory.java
+++ rpc-framework-common/src/main/java/github/javaguide/factory/SingletonFactory.java
@@ -3,6 +3,7 @@ package github.javaguide.factory;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 获取单例对象的工厂类
@@ -11,22 +12,32 @@ import java.util.Map;
  * @createTime 2020年06月03日 15:04:00
  */
 public final class SingletonFactory {
-    private static final Map<String, Object> OBJECT_MAP = new HashMap<>();
+    private static final Map<String, Object> OBJECT_MAP = new ConcurrentHashMap<>();
+    private static final Map<Class<?>, Object> LOCK_MAP = new ConcurrentHashMap<>();
 
     private SingletonFactory() {
     }
 
     public static <T> T getInstance(Class<T> c) {
+        if (c == null) {
+            throw new IllegalArgumentException();
+        }
         String key = c.toString();
-        Object instance;
-        synchronized (SingletonFactory.class) {
-            instance = OBJECT_MAP.get(key);
-            if (instance == null) {
-                try {
-                    instance = c.getDeclaredConstructor().newInstance();
-                    OBJECT_MAP.put(key, instance);
-                } catch (IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
-                    throw new RuntimeException(e.getMessage(), e);
+        Object instance = OBJECT_MAP.get(key);
+        
+        if (instance == null) {
+            synchronized (LOCK_MAP.computeIfAbsent(c, k -> new Object())) {
+                instance = OBJECT_MAP.get(key);
+                if (instance == null) {
+                    try {
+                        instance = c.getDeclaredConstructor().newInstance();
+                        OBJECT_MAP.put(key, instance);
+                    } catch (IllegalAccessException
+                            | InstantiationException
+                            | NoSuchMethodException
+                            | InvocationTargetException e) {
+                        throw new RuntimeException(e.getMessage(), e);
+                    }
                 }
             }
         }
