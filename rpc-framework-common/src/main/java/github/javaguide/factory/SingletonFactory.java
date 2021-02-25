52c5a4f7e00c38813540f3df68a49ebc6682e833
tangj1992 <694756023@qq.com>
2021-02-25 13:54:11 +0800

    修改单例获取工厂
    获取单例的时候不用加锁，当未获取到实例的时候才加锁生成实例

--- rpc-framework-common/src/main/java/github/javaguide/factory/SingletonFactory.java
+++ rpc-framework-common/src/main/java/github/javaguide/factory/SingletonFactory.java
@@ -18,7 +18,10 @@ public final class SingletonFactory {
 
     public static <T> T getInstance(Class<T> c) {
         String key = c.toString();
-        Object instance;
+        Object instance = OBJECT_MAP.get(key);
+        if (instance != null){
+            return c.cast(instance);
+        }
         synchronized (SingletonFactory.class) {
             instance = OBJECT_MAP.get(key);
             if (instance == null) {
